d4d4cd64db94a1022ab974b24e8ab9e582dbc42a
webOwl <nikitaterentev916@gmail.com>
2026-06-29 17:15:56 +0200

    feat: add language toggle button to header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { LucideGauge, LucideSun, LucideMoon } from '@lucide/angular';
 import { ThemeService } from '../services/theme.service';
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,5 @@ import { ThemeService } from '../services/theme.service';
 })
 export class Header {
   readonly theme = inject(ThemeService);
+  readonly translation = inject(TranslationService);
 }
